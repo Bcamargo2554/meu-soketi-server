ed93783b9c1c2f5237fd6862dcf6351be7dcf20b
Bcamargo2554 <45249496+Bcamargo2554@users.noreply.github.com>
2025-07-05 10:36:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const soketi = require('@soketi/soketi');
+const Soketi = require('@soketi/soketi');
 
-// Configura e inicia o servidor Soketi
-const instance = soketi.server({
-    // Render fornecerá a porta através de uma variável de ambiente
+// A forma correta de iniciar é usando "new Soketi(...)"
+const instance = new Soketi({
     port: process.env.PORT || 6001,
-
-    // Define o nosso aplicativo, usando as variáveis de ambiente que configuraremos no Render
     apps: [
         {
             id: process.env.SOKETI_APP_ID,
@@ -15,7 +12,8 @@ const instance = soketi.server({
     ],
 });
 
-// Inicia o servidor
+// Inicia o servidor e loga o sucesso
 instance.start().then(() => {
     console.log('Soketi server started successfully on Render!');
 });
+
